991053f0569d9475766f17ff163b0e75f97eafc6
skin547 <skin547yo@gmail.com>
2024-03-23 01:29:22 +0800

    refactor: make server.go use http-server and file storage

--- app/server.go
+++ app/server.go
@@ -5,18 +5,16 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
-	"strings"
+
+	"github.com/codecrafters-io/http-server-starter-go/internal"
 )
 
-const CRLF = "\r\n"
-
-var public string
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
+	var public string
 	flag.StringVar(&public, "directory", "./public", "files directory")
 	flag.Parse()
 	// if directory not exist, create one
@@ -40,6 +38,8 @@ func main() {
 		os.Exit(1)
 	}
 	defer l.Close()
+	fileStorage := internal.NewFileStorage(public)
+	httpServer := internal.NewHttpServer(fileStorage)
 	for {
 		conn, err := l.Accept()
 		if err != nil {
@@ -47,165 +47,6 @@ func main() {
 			conn.Close()
 			continue
 		}
-		go handle(conn)
+		go httpServer.Handle(conn)
 	}
-}
-
-func handle(conn net.Conn) {
-	defer conn.Close()
-
-	buf := make([]byte, 1024)
-	requestSize, err := conn.Read(buf)
-	if err != nil {
-		fmt.Println("Error reading connection: ", err.Error())
-		return
-	}
-	request := string(buf[:requestSize])
-
-	req := ParseRequest(request)
-	fmt.Printf("method: %s, path: %s, query: %s, version: %s\n", req.method, req.path, req.query, req.version)
-	fmt.Printf("headers: %v\n", req.headers)
-	fmt.Printf("body: %v\n", req.body)
-
-	res := Response{
-		body:       "",
-		version:    "HTTP/1.1",
-		statusCode: 200,
-		statusMsg:  "OK",
-		headers:    make(map[string]string),
-	}
-
-	switch {
-	case req.path == "/":
-	case strings.HasPrefix(req.path, "/echo/"):
-		splitedPath := strings.Split(req.path, "/echo/")
-		echo := splitedPath[1]
-
-		res.body = echo
-		res.headers["Content-Type"] = "text/plain"
-	case strings.HasPrefix(req.path, "/user-agent"):
-		userAgent, exist := req.headers["User-Agent"]
-		if !exist {
-			userAgent = "Unknown"
-		}
-		res.body = userAgent
-
-		res.headers["Content-Type"] = "text/plain"
-	case req.method == "GET" && strings.HasPrefix(req.path, "/files/"):
-		splitedPath := strings.Split(req.path, "/files/")
-		fileName := splitedPath[1]
-		fmt.Printf("fileName: %s\n", fileName)
-		// check if file exist
-		filePath := fmt.Sprintf("%s/%s", public, fileName)
-		if _, err := os.Stat(filePath); os.IsNotExist(err) {
-			res.statusCode = 404
-			res.statusMsg = "Not Found"
-			break
-		}
-
-		data, err := readFile(filePath)
-		if err != nil {
-			res.statusCode = 500
-			res.statusMsg = "Internal Server Error"
-			break
-		}
-		res.body = string(data)
-		res.headers["Content-Type"] = "application/octet-stream"
-	case req.method == "POST" && strings.HasPrefix(req.path, "/files/"):
-		splitedPath := strings.Split(req.path, "/files/")
-		fileName := splitedPath[1]
-		filePath := fmt.Sprintf("%s/%s", public, fileName)
-		err = os.WriteFile(filePath, []byte(req.body), 0644)
-		if err != nil {
-			fmt.Printf("Error writing file: %s\n", err)
-			res.statusCode = 500
-			res.statusMsg = "Internal Server Error"
-			break
-		}
-		res.body = "ok"
-		res.statusCode = 201
-	default:
-		res.statusCode = 404
-		res.statusMsg = "Not Found"
-	}
-
-	conn.Write([]byte(res.Serialize()))
-}
-
-func readFile(filePath string) ([]byte, error) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		return nil, fmt.Errorf("error opening file: %s", err)
-	}
-	defer file.Close()
-
-	data := make([]byte, 1024)
-	size, err := file.Read(data)
-	if err != nil {
-		return nil, fmt.Errorf("error reading file: %s", err)
-	}
-	return data[:size], nil
-}
-
-// path contain path and query string
-func splitPath(path string) (string, string) {
-	splited := strings.Split(path, "?")
-	if len(splited) == 1 {
-		return splited[0], ""
-	}
-	return splited[0], splited[1]
-}
-
-type Request struct {
-	method  string
-	path    string
-	query   string
-	version string
-	headers map[string]string
-	body    string
-}
-
-func ParseRequest(request string) Request {
-	lines := strings.Split(request, CRLF)
-	requestLine := strings.Split(lines[0], " ")
-	method := requestLine[0]
-	headers := make(map[string]string)
-
-	if len(lines) > 1 {
-		for _, line := range lines[1:] {
-			splited := strings.Split(line, ": ")
-			if len(splited) == 2 {
-				key := splited[0]
-				value := splited[1]
-				headers[key] = value
-			}
-		}
-	}
-
-	paths, query := splitPath(requestLine[1])
-	version := requestLine[2]
-	body := ""
-	if method == "POST" {
-		body = lines[len(lines)-1]
-		headers["Content-Length"] = strconv.Itoa(len(body))
-	}
-	return Request{method, paths, query, version, headers, body}
-}
-
-type Response struct {
-	version    string
-	statusCode int
-	statusMsg  string
-	headers    map[string]string
-	body       string
-}
-
-func (r *Response) Serialize() string {
-	r.headers["Content-Length"] = strconv.Itoa(len(r.body))
-	headers := ""
-
-	for key, value := range r.headers {
-		headers += fmt.Sprintf("%s: %s%s", key, value, CRLF)
-	}
-	return fmt.Sprintf("%s %d %s%s%s%s%s", r.version, r.statusCode, r.statusMsg, CRLF, headers, CRLF, r.body)
 }
