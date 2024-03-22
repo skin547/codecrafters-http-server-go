baf3e5309b60602c23eebb022ca0f597986ee90a
skin547 <skin547yo@gmail.com>
2024-03-23 00:06:24 +0800

    fix: create public directory if it is not exist

--- app/server.go
+++ app/server.go
@@ -18,6 +18,20 @@ func main() {
 
 	flag.StringVar(&public, "directory", "./public", "files directory")
 	flag.Parse()
+	// if directory not exist, create one
+	_, err := os.Stat(public)
+	if err != nil {
+		if os.IsNotExist(err) {
+			err = os.MkdirAll(public, 0755)
+			if err != nil {
+				fmt.Println("Error creating directory: ", err.Error())
+				os.Exit(1)
+			}
+		} else {
+			fmt.Println("Error checking directory: ", err.Error())
+			os.Exit(1)
+		}
+	}
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
